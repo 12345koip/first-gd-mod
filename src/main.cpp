9b7cbdf8d61e1082dbe936096e1afe1ae7e12ab4
12345koip <piok8393@gmail.com>
2026-02-23 22:09:50 +0000

    very useful mod trust me

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,29 @@
 //silly test mod idk
 
 #include <Geode/Geode.hpp>
+#include <Geode/modify/PlayLayer.hpp>
 
 using namespace geode::prelude;
 
-$on_mod(Loaded) {
-    FLAlertLayer::create(
-        "Hello!",
-        "testing ok thanks",
-        "OK"
-    )->show();
-}
+// $on_mod(Loaded) {
+//     FLAlertLayer::create(
+//         "Hello!",
+//         "testing ok thanks",
+//         "OK"
+//     )->show();
+// }
+
+class $modify(PlayLayer) {
+    void destroyPlayer(PlayerObject* player, GameObject* object) {
+        static int dc = 0;
+        PlayLayer::destroyPlayer(player, object);
+
+        if (dc++ != 0) {
+            FLAlertLayer::create(
+                "death",
+                "you died. please get better at the game.",
+                "OK"
+            )->show();
+        }
+    }
+};
