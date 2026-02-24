33fac1886490805b929bc4266182fbc2004b7a96
12345koip <piok8393@gmail.com>
2026-02-24 19:37:16 +0000

    wtf

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,22 @@
-//silly test mod idk
+//why are you using this mod
+//why
+//ITS LITERALLY 17 LINES FOR THE MOST USELESS THING!!!
 
 #include <Geode/Geode.hpp>
 #include <Geode/modify/PlayLayer.hpp>
 
 using namespace geode::prelude;
 
-// $on_mod(Loaded) {
-//     FLAlertLayer::create(
-//         "Hello!",
-//         "testing ok thanks",
-//         "OK"
-//     )->show();
-// }
-
 class $modify(PlayLayer) {
     void destroyPlayer(PlayerObject* player, GameObject* object) {
-        static int dc = 0;
-        PlayLayer::destroyPlayer(player, object);
 
-        if (dc++ != 0) {
-            FLAlertLayer::create(
-                "death",
-                "you died. please get better at the game.",
-                "OK"
-            )->show();
+        if (object != m_anticheatSpike) {
+            FLAlertLayer::create("death", "you died. please get better at the game.", "OK")->show();
+
+            PlayLayer* playLayer = PlayLayer::get(); //always exists in this context
+            playLayer->pauseGame(false);
         }
+
+        return PlayLayer::destroyPlayer(player, object);
     }
 };
